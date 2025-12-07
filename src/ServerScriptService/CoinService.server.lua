17ac841f3451bb7975bf53d2af42add71739e530
jeremytenjo <tenjojeremy@gmail.com>
2025-12-07 14:42:15 -0500

    Refactor CoinService: streamline coin collection logic and enhance player coin updates

--- src/ServerScriptService/CoinService.server.lua
+++ src/ServerScriptService/CoinService.server.lua
@@ -1,11 +1,26 @@
 -- Initializing services and variables
 local Workspace = game:GetService('Workspace')
 local Players = game:GetService('Players')
+local ServerStorage = game:GetService('ServerStorage')
+
+-- Modules
+local Leaderboard = require(ServerStorage.Leaderboard)
+local PlayerData = require(ServerStorage.PlayerData)
 
 local coinsFolder = Workspace.World.Coins
 local coins = coinsFolder:GetChildren()
 
+local COIN_KEY_NAME = PlayerData.COIN_KEY_NAME
 local COOLDOWN = 10
+local COIN_AMOUNT_TO_ADD = 1
+
+local function updatePlayerCoins(player, updateFunction)
+  -- Update the coin table
+  local newCoinAmount = PlayerData.updateValue(player, COIN_KEY_NAME, updateFunction)
+
+  -- Update the coin leaderboard
+  Leaderboard.setStat(player, COIN_KEY_NAME, newCoinAmount)
+end
 
 -- Defining the event handler
 local function onCoinTouched(otherPart, coin)
@@ -16,7 +31,12 @@ local function onCoinTouched(otherPart, coin)
       -- Player touched a coin
       coin.Transparency = 1
       coin:SetAttribute('Enabled', false)
-      print('Player collected coin')
+
+      updatePlayerCoins(player, function(oldCoinAmount)
+        oldCoinAmount = oldCoinAmount or 0
+        return oldCoinAmount + COIN_AMOUNT_TO_ADD
+      end)
+
       task.wait(COOLDOWN)
       coin.Transparency = 0
       coin:SetAttribute('Enabled', true)
