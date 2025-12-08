69613c7ca622395672fb3bb8d2a51d8c569da912
jeremytenjo <tenjojeremy@gmail.com>
2025-12-07 20:48:17 -0500

    refactor: Simplify coin touch event handling for improved readability

--- src/ServerScriptService/CoinService.server.ts
+++ src/ServerScriptService/CoinService.server.ts
@@ -1,41 +1,35 @@
-// Initializing services and variables
-const Workspace = game.GetService('Workspace')
-const Players = game.GetService('Players')
-
 // Modules
 import PlayerData from '../ServerStorage/PlayerData'
 
+const Workspace = game.GetService('Workspace')
+const Players = game.GetService('Players')
+
 const coinsFolder = Workspace.WaitForChild('World').WaitForChild('Coins') as Folder
 const coins = coinsFolder.GetChildren() as BasePart[]
-
-// Defining the event handler
-function onCoinTouched(otherPart: BasePart, coin: BasePart): void {
-  if (coin.GetAttribute('Enabled')) {
-    const character = otherPart.Parent
-    const player = Players.GetPlayerFromCharacter(character)
-
-    if (player) {
-      // Player touched a coin
-      coin.Transparency = 1
-      coin.SetAttribute('Enabled', false)
-
-      PlayerData.updateValue({
-        player,
-        key: PlayerData.COIN_KEY_NAME,
-      })
-
-      task.wait(10)
-      coin.Transparency = 0
-      coin.SetAttribute('Enabled', true)
-    }
-  }
-}
 
 // Setting up event listeners
 for (const coin of coins) {
   const part = coin
   part.SetAttribute('Enabled', true)
-  part.Touched.Connect((otherPart: BasePart) => {
-    onCoinTouched(otherPart, part)
+  part.Touched.Connect((otherPart) => {
+    if (coin.GetAttribute('Enabled')) {
+      const character = otherPart.Parent
+      const player = Players.GetPlayerFromCharacter(character)
+
+      if (player) {
+        // Player touched a coin
+        coin.Transparency = 1
+        coin.SetAttribute('Enabled', false)
+
+        PlayerData.updateValue({
+          player,
+          key: PlayerData.COIN_KEY_NAME,
+        })
+
+        task.wait(10)
+        coin.Transparency = 0
+        coin.SetAttribute('Enabled', true)
+      }
+    }
   })
 }
