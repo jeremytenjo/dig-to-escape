817b701c876e38992dce6a677ebfd97c92e4ceb5
jeremytenjo <tenjojeremy@gmail.com>
2025-12-08 22:25:28 -0500

    refactor: streamline jump button click handler for improved readability

--- src/ReplicatedStorage/jumpButtonClickHandler/jumpButtonClickHandler.server.ts
+++ src/ReplicatedStorage/jumpButtonClickHandler/jumpButtonClickHandler.server.ts
@@ -2,33 +2,31 @@ import { Players } from '@rbxts/services'
 
 import getInstance from '../utils/getInstance/getInstance.js'
 
-export default function jumpButtonClickHandler() {
-  const player = Players.LocalPlayer
-  const playerGui = player.FindFirstChild('PlayerGui') as PlayerGui
+const player = Players.LocalPlayer
+const playerGui = player.FindFirstChild('PlayerGui') as PlayerGui
 
-  const increaseJumpPowerFunction = getInstance<RemoteFunction>({
-    instancePath: 'ReplicatedStorage/Instances/IncreaseJumpPowerFunction',
+const increaseJumpPowerFunction = getInstance<RemoteFunction>({
+  instancePath: 'ReplicatedStorage/Instances/IncreaseJumpPowerFunction',
+})
+const jumpPurchaseGui = getInstance<Frame>({
+  instancePath: 'ReplicatedStorage/Instances/JumpPurchaseGui',
+})
+const jumpButton = jumpPurchaseGui.WaitForChild('JumpButton') as GuiButton
+
+function onButtonClicked() {
+  const [success, purchased] = pcall<[], unknown>(() => {
+    return increaseJumpPowerFunction.InvokeServer()
   })
-  const jumpPurchaseGui = getInstance<Frame>({
-    instancePath: 'ReplicatedStorage/Instances/JumpPurchaseGui',
-  })
-  const jumpButton = jumpPurchaseGui.WaitForChild('JumpButton') as GuiButton
 
-  function onButtonClicked() {
-    const [success, purchased] = pcall<[], unknown>(() => {
-      return increaseJumpPowerFunction.InvokeServer()
-    })
-
-    if (!success) {
-      // purchased will be the error message if success is false
-      error(tostring(purchased))
-    } else if (success && !purchased) {
-      warn('Not enough coins!')
-    }
+  if (!success) {
+    // purchased will be the error message if success is false
+    error(tostring(purchased))
+  } else if (success && !purchased) {
+    warn('Not enough coins!')
   }
-
-  jumpButton.Activated.Connect(onButtonClicked)
-
-  // Add the JumpPurchaseGui to the player's Gui
-  jumpPurchaseGui.Parent = playerGui
 }
+
+jumpButton.Activated.Connect(onButtonClicked)
+
+// Add the JumpPurchaseGui to the player's Gui
+jumpPurchaseGui.Parent = playerGui
